04e92815074803d121efb41d3600b013126616b3
Shawn Catoe <scatoe@paypal.com>
2016-02-10 15:44:41 -0600

    minified the controller.

--- controllers/index.js
+++ controllers/index.js
@@ -1,27 +1,15 @@
 'use strict';
 
-var http = require('http');
-
-var IndexModel = require('../models/index');
-var wunderground = require('../lib/wunder');
-var nest = require('../lib/nest').init('poop', 'pee');
 var dao = require('../lib/dao');
-
 
 module.exports = function (router) {
 
     router.get('/', function (req, res) {
-        wunderground.currentConditions('pws:KTXAUSTI186', function(weatherModel) {
-            console.log('nesting');
-            nest.getAllData(function(nestModel) {
-                var model = new IndexModel(weatherModel, nestModel);
-                res.render('index', model);
-            });
-        });
+        res.render('index', {});
     });
 
 
-    router.get('/test', function (req, res) {
+    router.get('/writeData', function (req, res) {
         dao.recordData();
         res.send(200);
     });
